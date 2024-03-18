20dbff1867d1c1592da093e34a16ff7ddb0d58d9
BrutalWizard <bru74lw1z4rd@gmail.com>
2024-03-18 05:12:02 +0300

    Added Example controllers.

---
+++ src/sources/PWLottieControllers/PWLottieIconController.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) PrivateWeb Software (https://github.com/PrivateWebSoftware) - All Rights Reserved
+ *
+ * Licensed under the Apache License 2.0 (the "License"). You may not use
+ * this file except in compliance with the License. You can obtain a copy
+ * in the file LICENSE in the source distribution
+ *
+ * Written by PrivateWeb Software <privatewebsoftware@protonmail.com>, July 2024
+ */
+
+#include "include/PWLottieControllers/PWLottieIconController.h"
+
+PWLottieIconController::PWLottieIconController(QObject* parent)
+    : PWLottieAbstractController { parent }
+{
+}
