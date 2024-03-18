cc37c37643872c905ce65a8c7825bf5156c5da9b
BrutalWizard <bru74lw1z4rd@gmail.com>
2024-03-18 05:14:00 +0300

    Removed unnecessary.

--- src/sources/PWLottieControllers/PWLottieBaseController.cpp
+++ src/sources/PWLottieControllers/PWLottieBaseController.cpp
@@ -10,8 +10,6 @@
 
 #include "include/PWLottieControllers/PWLottieBaseController.h"
 
-#include <QDebug>
-
 PWLottieBaseController::PWLottieBaseController(QObject* parent)
     : PWLottieAbstractController { parent }
 {
